954d981828c5ae24523973d9a51728f19b3f7278
gi8 <giottolino@gmail.com>
2021-07-30 15:29:17 +0200

    fix logic

--- main.py
+++ main.py
@@ -1,48 +1,67 @@
 import argparse
 import sys
+from typing import List
+
 import yaml
 
 
-def is_namespaced_kubernetes_manifest(filename):
+def is_kubernetes_manifest(filename: str) -> List[dict]:
     try:
-        with open(filename) as stream:
-            manifest = yaml.safe_load(stream)
+        with open(file=filename, mode="r") as stream:
+            manifests = list(yaml.load_all(stream=stream,
+                                           Loader=yaml.SafeLoader))
     except Exception as e:
-        return None
-    if not manifest.get("apiVersion"):
-        return None
+        return []
+    for manifest in manifests:
+      if not manifest.get("apiVersion"):
+          return []
+      return manifests
+
+def is_namespaced_kubernetes_manifest(manifest: dict) -> bool:
     if not manifest.get("metadata"):
-        return None
+        return False
     if not manifest["metadata"].get("namespace"):
-        return None
-    return manifest
+        return False
+    return True
 
+def is_ignored_kubernetes_kind(manifest: dict, ignored_kinds: List[str] = []) -> bool:
+    if not ignored_kinds:
+      return False
 
-def is_ignored_kubernetes_kind(manifest, ignored_kinds):
     if manifest.get("kind", "invalid_value") not in ignored_kinds:
         return False
     return True
 
 
-def main(argv=None):
+def main(argv: List = None):
     parser = argparse.ArgumentParser()
-    parser.add_argument('filenames', nargs='*', help='filenames to check')
-    parser.add_argument('--ignored-kinds', help='kinds to ignore (comma separated)', dest="ignored_kinds",
-                        metavar="Kind", default="")
+    parser.add_argument('filenames',
+                        nargs='*',
+                        help='filenames to check')
+    parser.add_argument('--ignored-kinds', '-i',
+                        help='kinds to ignore (comma separated)',
+                        dest="ignored_kinds",
+                        metavar="Kind",
+                        default="")
     args = parser.parse_args(argv)
     ignored_kinds = [k.strip() for k in args.ignored_kinds.split(",")]
 
     return_code = 0
     for fname in args.filenames:
-        manifest = is_namespaced_kubernetes_manifest(filename=fname)
-        if not manifest:
-            print(f"")
-        if not is_namespaced_kubernetes_manifest(filename=fname):
-            print(f"Kubernetes manifest missing namespace: {invalid_manifest_file}")
-            return_code = 1
-            continue
+        manifests = is_kubernetes_manifest(filename=fname)
 
-        if not is_ignored_kubernetes_kind(manifest=manifest, )
+        for manifest in manifests:
+          if not manifest:
+              continue
+
+          if not is_ignored_kubernetes_kind(manifest=manifest,
+                                            ignored_kinds=ignored_kinds):
+              continue
+
+          if not is_namespaced_kubernetes_manifest(manifest=manifest):
+              print(f"Kubernetes manifest missing namespace: {fname}")
+              return_code = 1
+              continue
 
     return return_code
 
